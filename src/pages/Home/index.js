c440faa386b61b5d906626fbffe4591c05369b92
Joao Carser <joaogabriel-14@hotmail.com>
2023-06-18 21:30:13 -0300

    ItemList criada, escopo de repositorio criado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import background from '../../assets/background.png';
+import ItemList from '../../components/ItemList'
 import './styles.css';
 
 
@@ -11,11 +12,26 @@ function App() {
       <div className="conteudo">
         <img src={background} className="background" alt="background app" />
 
-
+        
         <div className="info">
           <div>
             <input name="usuario" placeholder="@username"/>
             <button>Buscar</button>
+          </div>
+          <div className="perfil">
+            <img src="https://avatars.githubusercontent.com/u/48493500?s=400&u=e93888c27bf117c57f640f4dc6c991fe200fc458&v=4" className="profile" alt="avatar"/>
+            <div>
+              <h3>João Carser</h3>
+              <span>@JoaoCarser</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr/>
+          <div>
+            <h4 className="repositorio">Repositórios</h4>
+            <ItemList title="Teste1" description="teste de descrição"/>
+            <ItemList title="Teste1" description="teste de descrição"/>
+            <ItemList title="Teste1" description="teste de descrição"/>
           </div>
         </div>
       </div>
